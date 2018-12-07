0d755024d5cca4266a9141f33f6ffe1fdd73c40c
dongwei.dq <dongwei.dq@alibaba-inc.com>
2018-12-07 16:53:04 +0800

    init commit

---
+++ personalweb-wasm/src/main/java/com/ranttu/rapid/personalweb/core/wasm/misc/$.java
@@ -0,0 +1,17 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.ranttu.rapid.personalweb.core.wasm.misc;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * common utilities
+ *
+ * @author rapid
+ * @version $Id: $.java, v 0.1 2018Äê12ÔÂ07ÈÕ 4:41 PM rapid Exp $
+ */
+@UtilityClass
+public class $ {
+}
